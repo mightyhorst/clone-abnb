6d78afe992dbaf24370d5493bc1e77e1a3f61fd0
Nick Mitchell <nickymitch@gmail.com>
2021-04-23 17:28:36 +1000

    updated drop down menu css

--- src/uikit/DropDown/DropDownMenu/DropDownMenu.tsx
+++ src/uikit/DropDown/DropDownMenu/DropDownMenu.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './DropDownMenu.css';
+import clsx from 'clsx';
 
 interface IDropDownMenu{
     children?: React.ReactNode;
+    isOpen?: boolean;
 }
 
-export function DropDownMenu({children}: IDropDownMenu){
+export function DropDownMenu({children, isOpen = true}: IDropDownMenu){
     return (<>
-        <ul className='DropDownMenu'>
+        <ul className={clsx('DropDownMenu', {'isOpen': isOpen})}>
             {children}
         </ul>
     </>);
