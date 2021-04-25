64e63a5c88474c8c8540cc55fcd86f7fd1587b17
Nick Mitchell <nickymitch@gmail.com>
2021-04-25 11:43:23 +0500

    feat(trips): update trips page with tabs

--- src/pages/TripsPage/TripsPage.tsx
+++ src/pages/TripsPage/TripsPage.tsx
@@ -2,168 +2,174 @@ import React from 'react';
 import './TripsPage.css';
 
 import { Layout } from '../Layout';
-import {
-    Card,
-} from '../../uikit';
+import { Tab, Card } from '../../uikit';
 
-interface ITripsPage {
-}
+interface ITripsPage {}
+
+const upcomingTab = (<>
+    todo
+</>);
+
+const pastTab = (
+    <>
+        <Card.Container>
+            <Card>
+                <Card.Image imgSrc="/img/placeholder.png" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="/img/placeholder.png" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+            <Card>
+                <Card.Image imgSrc="https://via.placeholder.com/400" />
+                <Card.Title
+                    subTitle="28 April 2020 - 29 April 2020"
+                    title={'Auckland'}
+                />
+                <Card.List>
+                    <Card.List.Item href="#">
+                        Oneroa Private Oasis
+                    </Card.List.Item>
+                </Card.List>
+                <Card.Footer href="#">Show more trip plans</Card.Footer>
+            </Card>
+        </Card.Container>
+    </>
+);
 
 export function TripsPage(props: ITripsPage) {
-    return (<>
-        <Layout.Standard pageName='TripsPage'>
-            <Card.Container>
-                <Card>
-                    <Card.Image imgSrc='/img/placeholder.png' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='/img/placeholder.png' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Image imgSrc='https://via.placeholder.com/400' />
-                    <Card.Title 
-                        subTitle='28 April 2020 - 29 April 2020'
-                        title={'Auckland'}
-                    />
-                    <Card.List>
-                        <Card.List.Item href='#'>
-                            Oneroa Private Oasis
-                        </Card.List.Item>
-                    </Card.List>
-                    <Card.Footer href='#'>
-                        Show more trip plans
-                    </Card.Footer>
-                </Card>
-            </Card.Container>
-        </Layout.Standard>
-    </>);
+    return (
+        <>
+            <Layout.Standard pageName="TripsPage">
+                <Tab.Layout>
+                    <Tab.Container>
+                        <Tab>
+                            Upcoming
+                        </Tab>
+                        <Tab isActive>
+                            Past
+                        </Tab>
+                    </Tab.Container>
+                    <Tab.Panel.Container>
+                        <Tab.Panel>
+                            {upcomingTab}
+                        </Tab.Panel>
+                        <Tab.Panel>
+                            {pastTab}
+                        </Tab.Panel>
+                    </Tab.Panel.Container>
+                </Tab.Layout>
+            </Layout.Standard>
+        </>
+    );
 }
