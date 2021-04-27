7d5e05b660c6e06237f57789b3582ca2e980017e
Nick Mitchell <nickymitch@gmail.com>
2021-04-27 14:51:43 +0500

    fix: fonts

--- src/components/Footer/FooterBottom/FooterBottom.tsx
+++ src/components/Footer/FooterBottom/FooterBottom.tsx
@@ -4,6 +4,54 @@ import "./FooterBottom.css";
 interface IFooterBottom {
   children?: React.ReactNode;
 }
+
+function FooterBottomLeft(props: IFooterBottom) {
+  return (
+    <>
+      <div className="FooterBottomLeft">
+        <ul>
+          <li>&copy; 2021 AirBnB</li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+          <li>
+            <a href="#">Terms</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function FooterBottomRight(props: IFooterBottom) {
+  return (
+    <>
+      <div className="FooterBottomRight">
+        <ul>
+          <li>
+            <a href="#">English (AU)</a>
+          </li>
+          <li>
+            <a href="#">$ AUD</a>
+          </li>
+          <li>
+            <a href="#">Facebook Icon</a>
+          </li>
+          <li>
+            <a href="#">Twitter Icon</a>
+          </li>
+          <li>
+            <a href="#">Insta Icon</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
 export function FooterBottom(props: IFooterBottom) {
   return (
     <>
@@ -13,49 +61,6 @@ export function FooterBottom(props: IFooterBottom) {
           <FooterBottomLeft />
         </div>
       </footer>
-    </>
-  );
-}
-
-function FooterBottomLeft(props: IFooterBottom) {
-  return (
-    <>
-      <ul className="FooterBottomLeft">
-        <li>&copy; 2021 AirBnB</li>
-        <li>
-          <a href="#">Privacy</a>
-        </li>
-        <li>
-          <a href="#">Terms</a>
-        </li>
-        <li>
-          <a href="#">Sitemap</a>
-        </li>
-      </ul>
-    </>
-  );
-}
-
-function FooterBottomRight(props: IFooterBottom) {
-  return (
-    <>
-      <ul className="FooterBottomRight">
-        <li>
-          <a href="#">English (AU)</a>
-        </li>
-        <li>
-          <a href="#">$ AUD</a>
-        </li>
-        <li>
-          <a href="#">Facebook Icon</a>
-        </li>
-        <li>
-          <a href="#">Twitter Icon</a>
-        </li>
-        <li>
-          <a href="#">Insta Icon</a>
-        </li>
-      </ul>
     </>
   );
 }
