932ad28a5e20d086e83126280ba85a4429d24919
Nick Mitchell <nickymitch@gmail.com>
2021-04-28 05:33:38 +0500

    tab css

--- src/pages/TripsPage/TripsPage.tsx
+++ src/pages/TripsPage/TripsPage.tsx
@@ -12,36 +12,75 @@ const pastTab = (
   <>
     <Card.Container>
       <Card>
-        <Card.Image imgSrc="/img/placeholder.png" />
-        <Card.Title
-          subTitle="28 April 2020 - 29 April 2020"
-          title={"Auckland"}
-        />
-        <Card.List>
-          <Card.List.Item href="#">Oneroa Private Oasis</Card.List.Item>
-        </Card.List>
+        <Card.Image imgSrc="/img/2f13349d-879d-43c6-83e3-8e5679291d53.jpg" />
+        <div className="card-content">
+          <Card.Title
+            subTitle="28 April 2020 - 29 April 2020"
+            title={"Auckland"}
+          />
+          <Card.List>
+            <Card.List.Item href="#">Oneroa Private Oasis</Card.List.Item>
+          </Card.List>
+        </div>
         <Card.Footer href="#">Show more trip plans</Card.Footer>
       </Card>
       <Card>
-        <Card.Image imgSrc="/img/placeholder.png" />
-        <Card.Title
-          subTitle="28 April 2020 - 29 April 2020"
-          title={"Auckland"}
-        />
-        <Card.List>
-          <Card.List.Item href="#">Oneroa Private Oasis</Card.List.Item>
-        </Card.List>
+        <Card.Image imgSrc="/img/36f53e61-db8d-403c-9122-5b761c0e4264.jpg" />
+        <div className="card-content">
+          <Card.Title
+            subTitle="28 April 2020 - 29 April 2020"
+            title={"Auckland"}
+          />
+          <Card.List>
+            <Card.List.Item href="#">Brand New & Ready For You</Card.List.Item>
+          </Card.List>
+        </div>
         <Card.Footer href="#">Show more trip plans</Card.Footer>
       </Card>
       <Card>
-        <Card.Image imgSrc="/img/placeholder.png" />
-        <Card.Title
-          subTitle="28 April 2020 - 29 April 2020"
-          title={"Auckland"}
-        />
-        <Card.List>
-          <Card.List.Item href="#">Oneroa Private Oasis</Card.List.Item>
-        </Card.List>
+        <Card.Image imgSrc="/img/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg" />
+        <div className="card-content">
+          <Card.Title subTitle="07 jan 2020 - 09 jan 2020" title={"Piha"} />
+          <Card.List>
+            <Card.List.Item href="#">Piha Beach Studio</Card.List.Item>
+          </Card.List>
+        </div>
+        <Card.Footer href="#">Show more trip plans</Card.Footer>
+      </Card>
+      <Card>
+        <Card.Image imgSrc="/img/2f13349d-879d-43c6-83e3-8e5679291d53.jpg" />
+        <div className="card-content">
+          <Card.Title
+            subTitle="28 April 2020 - 29 April 2020"
+            title={"Auckland"}
+          />
+          <Card.List>
+            <Card.List.Item href="#">Oneroa Private Oasis</Card.List.Item>
+          </Card.List>
+        </div>
+        <Card.Footer href="#">Show more trip plans</Card.Footer>
+      </Card>
+      <Card>
+        <Card.Image imgSrc="/img/36f53e61-db8d-403c-9122-5b761c0e4264.jpg" />
+        <div className="card-content">
+          <Card.Title
+            subTitle="28 April 2020 - 29 April 2020"
+            title={"Auckland"}
+          />
+          <Card.List>
+            <Card.List.Item href="#">Brand New & Ready For You</Card.List.Item>
+          </Card.List>
+        </div>
+        <Card.Footer href="#">Show more trip plans</Card.Footer>
+      </Card>
+      <Card>
+        <Card.Image imgSrc="/img/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg" />
+        <div className="card-content">
+          <Card.Title subTitle="07 jan 2020 - 09 jan 2020" title={"Piha"} />
+          <Card.List>
+            <Card.List.Item href="#">Piha Beach Studio</Card.List.Item>
+          </Card.List>
+        </div>
         <Card.Footer href="#">Show more trip plans</Card.Footer>
       </Card>
     </Card.Container>
