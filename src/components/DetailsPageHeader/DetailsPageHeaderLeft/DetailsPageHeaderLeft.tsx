c3a391af40f09f59d4cb1e5c45643e70b6b2ffd5
Nick Mitchell <nickymitch@gmail.com>
2021-05-10 18:50:22 +1000

    chore: details page head

--- src/components/DetailsPageHeader/DetailsPageHeaderLeft/DetailsPageHeaderLeft.tsx
+++ src/components/DetailsPageHeader/DetailsPageHeaderLeft/DetailsPageHeaderLeft.tsx
@@ -3,26 +3,28 @@ import './DetailsPageHeaderLeft.css';
 import {Icon} from '../../../uikit';
 
 export function DetailsPageHeaderLeft(){
-    return (<div className='DetailsPageHeaderLeft'>
-        <span className='page-header-left__star'>
-            <Icon.Star />
-            <strong>
-                4.87
-            </strong>
-            <a href="#">
-                (36 reviews)
-            </a>
-        </span>
-        <span className='page-header-left__superhost '>
-            <Icon.Medal />
-            <div>
-                Superhost
-            </div>
-        </span>
-        <span className='page-header-left__location'>
-            <a href="#">
-                Great Barrier Island, New Zealand
-            </a>
-        </span>
-    </div>)
+    return (<>
+        <div className='DetailsPageHeaderLeft'>
+            <span className='page-header-left__star'>
+                <Icon.Star />
+                <strong>
+                    4.87
+                </strong>
+                <a href="#">
+                    (36 reviews)
+                </a>
+            </span>
+            <span className='page-header-left__superhost '>
+                <Icon.Medal />
+                <div>
+                    Superhost
+                </div>
+            </span>
+            <span className='page-header-left__location'>
+                <a href="#">
+                    Great Barrier Island, New Zealand
+                </a>
+            </span>
+        </div>
+    </>)
 }
