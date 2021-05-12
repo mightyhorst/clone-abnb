b54c4311315dba5c66f0fb74518163032d1d0534
Nick Mitchell <nickymitch@gmail.com>
2021-05-12 12:44:19 +1000

    feat: BookingForm

--- src/uikit/BookingForm/BookingForm.tsx
+++ src/uikit/BookingForm/BookingForm.tsx
@@ -10,7 +10,13 @@ import './BookingForm.responsive.css';
 /**
  * @requires Components
  */
-import {} from '..';
+import {
+    Icon,
+    Button,
+    Row, 
+    Column as Col,
+    DropDown,
+} from '../../uikit';
 // import {} from '.';
 
 /**
@@ -27,15 +33,75 @@ export interface IBookingForm{
  * @returns {BookingForm}
  */
 export function BookingForm({children}: IBookingForm){
-    /**
-     * @constant className - build the css classes to add to the BookingForm
-     */
-    const className = clsx(
-        'BookingForm',
-    );
     return (<>
-        <div className={className}>
-            {children}
+        <div className='BookingForm'>
+            <header className="booking-form__header">
+                <div className="booking-form__header-price">
+                    $269 / night
+                </div>
+                <div className="booking-form__header-reviews">
+                    <Icon.Star />
+                    <strong>
+                        4.97
+                    </strong>
+                    <span>
+                        (
+                            <a href="#/reviews">
+                                36 reviews
+                            </a>
+                        )
+                    </span>
+                </div>
+            </header>
+            <form 
+                action="#" 
+                className="booking-form__form">
+                <fieldset>
+                    <Row>
+                        <Col.Half>
+                            <label htmlFor='booking-form__checkin'>
+                                <input 
+                                    id='booking-form__checkin'
+                                    type="text"
+                                    placeholder="Add date"
+                                />
+                            </label>
+                        </Col.Half>
+                        <Col.Half>
+                            <label htmlFor='booking-form__checkout'>
+                                <input 
+                                    id='booking-form__checkout'
+                                    type="text"
+                                    placeholder="Add date"
+                                />
+                            </label>
+                        </Col.Half>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <label htmlFor='booking-form__guests'>
+                                <select 
+                                    id='booking-form__guests'
+                                >
+                                    <option value="1" selected>
+                                        1 guest
+                                    </option>
+                                    <option value="2">
+                                        2 guests
+                                    </option>
+                                    <option value="3">
+                                        3 guests
+                                    </option>
+                                </select>
+                                <i className="fa fa-chevron-down"></i>
+                            </label>
+                        </Col>
+                    </Row>
+                </fieldset>
+                <Button variant='primary'>
+                    Check availability
+                </Button>
+            </form>
         </div>
     </>);
 }
