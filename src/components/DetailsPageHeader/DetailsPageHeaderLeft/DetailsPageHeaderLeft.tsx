bd4790239d3d0679d715b076f855bf76ca330591
Nick Mitchell <nickymitch@gmail.com>
2021-06-14 19:39:56 +0500

    new

--- src/components/DetailsPageHeader/DetailsPageHeaderLeft/DetailsPageHeaderLeft.tsx
+++ src/components/DetailsPageHeader/DetailsPageHeaderLeft/DetailsPageHeaderLeft.tsx
@@ -1,30 +1,27 @@
-import './DetailsPageHeaderLeft.css';
+import "./DetailsPageHeaderLeft.css";
 
-import {Icon} from '../../../uikit';
+import { Icon } from "../../../uikit";
 
-export function DetailsPageHeaderLeft(){
-    return (<>
-        <div className='DetailsPageHeaderLeft'>
-            <span className='page-header-left__star'>
-                <Icon.Star />
-                <strong>
-                    4.87
-                </strong>
-                <a href="#">
-                    (36 reviews)
-                </a>
-            </span>
-            <span className='page-header-left__superhost '>
-                <Icon.Medal />
-                <div>
-                    Superhost
-                </div>
-            </span>
-            <span className='page-header-left__location'>
-                <a href="#">
-                    Great Barrier Island, New Zealand
-                </a>
-            </span>
+export function DetailsPageHeaderLeft() {
+  return (
+    <>
+      <div className="DetailsPageHeaderLeft">
+        <div className="page-header-title">
+          <p>Waterfront Homestead at Blind Bay</p>
         </div>
-    </>)
+        <span className="page-header-left__star">
+          <Icon.Star />
+          <strong>4.87</strong>
+          <a href="#">(36 reviews)</a>
+        </span>
+        <span className="page-header-left__superhost ">
+          <Icon.Medal />
+          <div>Superhost</div>
+        </span>
+        <span className="page-header-left__location">
+          <a href="#">Great Barrier Island, New Zealand</a>
+        </span>
+      </div>
+    </>
+  );
 }
