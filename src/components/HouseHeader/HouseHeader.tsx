0b58c20f84a0ac9c22e21fc7f6ef4a26781b1da0
Nick Mitchell <nickymitch@gmail.com>
2021-06-17 16:52:06 +1000

    feat: booking form

--- src/components/HouseHeader/HouseHeader.tsx
+++ src/components/HouseHeader/HouseHeader.tsx
@@ -1,55 +1,47 @@
-import React from 'react';
+import React from "react";
 
 /**
  * @requires Styles
  */
-import './HouseHeader.css';
-import './HouseHeader.responsive.css';
+import "./HouseHeader.css";
+import "./HouseHeader.responsive.css";
 
 /**
  * @requires Components
  */
-import {
-    ImageProfile,
-} from '../../uikit';
+import { ImageProfile } from "../../uikit";
 
 /**
  * @interface IHouseHeader
  * @description HouseHeader props types
  */
-export interface IHouseHeader{
-}
+export interface IHouseHeader {}
 
 /**
  * @function HouseHeader
- * @param props - props for children 
+ * @param props - props for children
  * @returns {HouseHeader}
  */
-export function HouseHeader({}: IHouseHeader){
-    return (<>
-        <div className='HouseHeader'>
-            <div className="house-header__details">
-                <h2>
-                    Entire house hosted by Paul
-                </h2>
-                <ul className="home-header__subtitle">
-                    <li>
-                        6 guests
-                    </li>
-                    <li>
-                        3 bedrooms
-                    </li>
-                    <li>
-                        5 beds
-                    </li>
-                    <li>
-                        1 bath
-                    </li>
-                </ul>
-            </div>
-            <div className="house-header__profile">
-                <ImageProfile />
-            </div>
+export function HouseHeader({}: IHouseHeader) {
+  return (
+    <>
+      <div className="HouseHeader">
+        <div className="house-header__details">
+          <h2>Entire house hosted by Paul</h2>
+          <ul className="home-header__subtitle">
+            <li>6 guests</li>
+            <li> · </li>
+            <li>3 bedrooms</li>
+            <li> · </li>
+            <li>5 beds</li>
+            <li> · </li>
+            <li>1 bath</li>
+          </ul>
         </div>
-    </>);
+        <div className="house-header__profile">
+          <ImageProfile />
+        </div>
+      </div>
+    </>
+  );
 }
--- src/uikit/BookingForm/BookingForm.tsx
+++ src/uikit/BookingForm/BookingForm.tsx
@@ -1,108 +1,95 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 /**
  * @requires Styles
  */
-import './BookingForm.css';
-import './BookingForm.responsive.css';
+import "./BookingForm.css";
+import "./BookingForm.responsive.css";
 
 /**
  * @requires Components
  */
-import {
-    Icon,
-    Button,
-    Row, 
-    Column as Col,
-    DropDown,
-} from '../../uikit';
+import { Icon, Button, Row, Column as Col, DropDown } from "../../uikit";
 // import {} from '.';
 
 /**
  * @interface IBookingForm
  * @description BookingForm props types
  */
-export interface IBookingForm{
-    children?: React.ReactNode;
+export interface IBookingForm {
+  children?: React.ReactNode;
 }
 
 /**
  * @function BookingForm
- * @param props - props for children 
+ * @param props - props for children
  * @returns {BookingForm}
  */
-export function BookingForm({children}: IBookingForm){
-    return (<>
-        <div className='BookingForm'>
-            <header className="booking-form__header">
-                <div className="booking-form__header-price">
-                    $269 / night
-                </div>
+export function BookingForm({ children }: IBookingForm) {
+  return (
+    <>
+      <div className="booking-form-container">
+        <div className="BookingForm">
+          <div className="booking-form-body">
+            <div className="booking-form-content-section">
+              <header className="booking-form__header">
+                <div className="booking-form__header-price">$269 / night</div>
                 <div className="booking-form__header-reviews">
-                    <Icon.Star />
-                    <strong>
-                        4.97
-                    </strong>
-                    <span>
-                        (
-                            <a href="#/reviews">
-                                36 reviews
-                            </a>
-                        )
-                    </span>
+                  <i className="fa fa-star checked"></i>
+                  <strong>4.97</strong>
+                  <span>
+                    (<a href="#/reviews">36 reviews</a>)
+                  </span>
                 </div>
-            </header>
-            <form 
-                action="#" 
-                className="booking-form__form">
+              </header>
+              <form action="#" className="booking-form__form">
                 <fieldset>
-                    <Row>
-                        <Col.Half>
-                            <label htmlFor='booking-form__checkin'>
-                                <input 
-                                    id='booking-form__checkin'
-                                    type="text"
-                                    placeholder="Add date"
-                                />
-                            </label>
-                        </Col.Half>
-                        <Col.Half>
-                            <label htmlFor='booking-form__checkout'>
-                                <input 
-                                    id='booking-form__checkout'
-                                    type="text"
-                                    placeholder="Add date"
-                                />
-                            </label>
-                        </Col.Half>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <label htmlFor='booking-form__guests'>
-                                <select 
-                                    id='booking-form__guests'
-                                    defaultValue='1'
-                                >
-                                    <option value="1">
-                                        1 guest
-                                    </option>
-                                    <option value="2">
-                                        2 guests
-                                    </option>
-                                    <option value="3">
-                                        3 guests
-                                    </option>
-                                </select>
-                                <i className="fa fa-chevron-down"></i>
-                            </label>
-                        </Col>
-                    </Row>
+                  <Row>
+                    <Col.Half>
+                      <label htmlFor="booking-form__checkin">
+                        <input
+                          id="booking-form__checkin"
+                          type="text"
+                          placeholder="Add date"
+                        />
+                      </label>
+                    </Col.Half>
+                    <Col.Half>
+                      <label htmlFor="booking-form__checkout">
+                        <input
+                          id="booking-form__checkout"
+                          type="text"
+                          placeholder="Add date"
+                        />
+                      </label>
+                    </Col.Half>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <label htmlFor="booking-form__guests">
+                        <select id="booking-form__guests" defaultValue="1">
+                          <option value="1">1 guest</option>
+                          <option value="2">2 guests</option>
+                          <option value="3">3 guests</option>
+                        </select>
+                        <i className="fa fa-chevron-down"></i>
+                      </label>
+                    </Col>
+                  </Row>
                 </fieldset>
-                <Button variant='primary'>
-                    Check availability
-                </Button>
-            </form>
+                <Button variant="primary">Check availability</Button>
+              </form>
+            </div>
+            <div className="booking-from-footer">
+              <p>
+                Enter dates and number of guests to check the total trip price,
+                including additional fees and any taxes.
+              </p>
+            </div>
+          </div>
         </div>
-    </>);
+      </div>
+    </>
+  );
 }
