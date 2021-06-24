b9e84a9fd5be7d9122a6c78cab06d17e082e46cc
Nick Mitchell <nickymitch@gmail.com>
2021-06-25 03:27:17 +0500

    Review, Location, Detail sections

--- src/components/HouseReviews/HouseReviews.tsx
+++ src/components/HouseReviews/HouseReviews.tsx
@@ -1,119 +1,133 @@
-import React from 'react';
+import React from "react";
 
 /**
  * @requires Styles
  */
-import './HouseReviews.css';
-import './HouseReviews.responsive.css';
+import "./HouseReviews.css";
+import "./HouseReviews.responsive.css";
 
 /**
  * @requires Components
  */
-import {} from '../../components';
-import {
-    Review,
-    Rating,
-    Row,
-    Column as Col,
-    Button, 
-    Icon,
-} from '../../uikit';
+import {} from "../../components";
+import { Review, Rating, Row, Column as Col, Button, Icon } from "../../uikit";
 
 /**
  * @interface IHouseReviews
  * @description HouseReviews props types
  */
-export interface IHouseReviews{
-    children?: React.ReactNode;
+export interface IHouseReviews {
+  children?: React.ReactNode;
 }
 
 /**
  * @function HouseReviews
- * @param props - props for children 
+ * @param props - props for children
  * @returns {HouseReviews}
  */
-export function HouseReviews({children}: IHouseReviews){
-    return (<>
-        <section className='HouseReviews'>
-            <div className="house-reviews__summary">
-                <Icon.Star />
-                <div className="house-reviews__total-rating">
-                    4.97
-                </div>
-                <div className="house-reviews__total-reviews">
-                    (36 reviews)
-                </div>
-            </div>
-            <Row addClass='house-reviews__ratings'>
-                <Col.Half>
-                    <Rating 
-                        label='Cleanliness'
-                        rating={4.9}
-                        total={5}
-                    />
-                </Col.Half>
-                <Col.Half>
-                </Col.Half>
-            </Row>
-            <Row addClass='house-reviews__reviews'>
-                <Col.Half>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                </Col.Half>
-                <Col.Half>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                    <Review 
-                        profileImg='/img/profile.jpg'
-                        profileName='Kara'
-                        datePublished='April 2021'
-                        reviewId='100'
-                    >
-                        Idyllic spot, perfect for an old style Kiwi holiday, with your own private beach! We enjoyed kayaking, swimming, walking to the wharf and across the property to waterfalls and arou… 
-                    </Review>
-                </Col.Half>
-            </Row>
-            <div className="house-reviews__show-all-reviews">
-                <Button>
-                    Show all 36 Reviews
-                </Button>
-            </div>
-        </section>
-    </>);
+export function HouseReviews({ children }: IHouseReviews) {
+  return (
+    <>
+      <section className="HouseReviews">
+        <div className="house-reviews__summary">
+          <span className="fa fa-star checked"></span>
+          <div className="house-reviews__total-rating">4.97</div>
+          <div className="house-reviews__total-reviews">(36 reviews)</div>
+        </div>
+        <Row addClass="house-reviews__ratings">
+          <Col.Half>
+            <Rating label="Cleanliness" rating={4.9} total={5} />
+          </Col.Half>
+          <Col.Half>
+            <Rating label="Accuracy" rating={4.9} total={5} />
+          </Col.Half>
+        </Row>
+        <Row addClass="house-reviews__ratings">
+          <Col.Half>
+            <Rating label="Communication" rating={5.0} total={5} />
+          </Col.Half>
+          <Col.Half>
+            <Rating label="Location" rating={5.0} total={5} />
+          </Col.Half>
+        </Row>
+        <Row addClass="house-reviews__ratings">
+          <Col.Half>
+            <Rating label="Check-in" rating={5.0} total={5} />
+          </Col.Half>
+          <Col.Half>
+            <Rating label="Value" rating={4.8} total={5} />
+          </Col.Half>
+        </Row>
+        <Row addClass="house-reviews__reviews">
+          <Col.Half>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+          </Col.Half>
+          <Col.Half>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+            <Review
+              profileImg="/img/profile.jpg"
+              profileName="Kara"
+              datePublished="April 2021"
+              reviewId="100"
+            >
+              Idyllic spot, perfect for an old style Kiwi holiday, with your own
+              private beach! We enjoyed kayaking, swimming, walking to the wharf
+              and across the property to waterfalls and arou…
+            </Review>
+          </Col.Half>
+        </Row>
+        <div className="house-reviews__show-all-reviews">
+          <Button>Show all 36 Reviews</Button>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/ThingsToKnow/ThingsToKnow.tsx
+++ src/components/ThingsToKnow/ThingsToKnow.tsx
@@ -1,90 +1,90 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 /**
  * @requires Styles
  */
-import './ThingsToKnow.css';
-import './ThingsToKnow.responsive.css';
+import "./ThingsToKnow.css";
+import "./ThingsToKnow.responsive.css";
 
 /**
  * @requires Components
  */
-import {ThingsToKnowRow} from './ThingsToKnowRow';
-import {ThingsToKnowCol} from './ThingsToKnowCol';
-import {} from '../../uikit';
+import { ThingsToKnowRow } from "./ThingsToKnowRow";
+import { ThingsToKnowCol } from "./ThingsToKnowCol";
+import {} from "../../uikit";
 
 /**
  * @interface IThingsToKnow
  * @description ThingsToKnow props types
  */
-export interface IThingsToKnow{
-    children?: React.ReactNode;
+export interface IThingsToKnow {
+  children?: React.ReactNode;
 }
 
 /**
  * @function ThingsToKnow
- * @param props - props for children 
+ * @param props - props for children
  * @returns {ThingsToKnow}
  */
-export function ThingsToKnow({children}: IThingsToKnow){
-    return (<>
-        <div className='ThingsToKnow'>
-            <h2> Things to know </h2>
-            <ThingsToKnowRow>
-                <ThingsToKnowCol>
-                    <h3>
-                        House rules
-                    </h3>
-                    <ul>
-                        <li>
-                            <i className="fa fa-clock"></i>
-                            <span>
-                                Check in flexible
-                            </span>
-                        </li>
-                    </ul>
-                    <a href="#">
-                        <span>
-                            Show All
-                        </span>
-                        <i className="fa fa-chevron-right"></i>
-                    </a>
-                </ThingsToKnowCol>
-                <ThingsToKnowCol>
-                    <h3>
-                        Health &amp; Safety
-                    </h3>
-                    <ul>
-                        <li>
-                            <i className="fa fa-stars"></i>
-                            <span>
-                                Commited to AirBnB's enhanced
-                            </span>
-                        </li>
-                    </ul>
-                    <a href="#">
-                        <span>
-                            Show All
-                        </span>
-                        <i className="fa fa-chevron-right"></i>
-                    </a>
-                </ThingsToKnowCol>
-                <ThingsToKnowCol>
-                    <h3>
-                        Cancellation Policy
-                    </h3>
-                    <p>
-                        Add your trip dates
-                    </p>
-                    <a href="#">
-                        <span>
-                            Add Dates
-                        </span>
-                        <i className="fa fa-chevron-right"></i>
-                    </a>
-                </ThingsToKnowCol>
-            </ThingsToKnowRow>
-        </div>
-    </>);
+export function ThingsToKnow({ children }: IThingsToKnow) {
+  return (
+    <>
+      <div className="ThingsToKnow">
+        <h2> Things to know </h2>
+        <ThingsToKnowRow>
+          <ThingsToKnowCol>
+            <h3>House rules</h3>
+            <ul>
+              <li>
+                <svg
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="presentation"
+                  focusable="false"
+                >
+                  <path d="M16 1C7.716 1 1 7.716 1 16s6.716 15 15 15 15-6.716 15-15S24.284 1 16 1zm7.895 22.954l-9.526-5.5a1.5 1.5 0 0 1-.743-1.154l-.007-.145v-11h3V16.29l8.776 5.067-1.5 2.598z"></path>
+                </svg>
+                <span>Check in flexible</span>
+              </li>
+            </ul>
+            <a href="#">
+              <span>Show All</span>
+              <i className="fa fa-chevron-right"></i>
+            </a>
+          </ThingsToKnowCol>
+          <ThingsToKnowCol>
+            <h3>Health &amp; Safety</h3>
+            <ul>
+              <li>
+                <svg
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  role="presentation"
+                  focusable="false"
+                >
+                  <path d="M16 1C7.716 1 1 7.716 1 16s6.716 15 15 15 15-6.716 15-15S24.284 1 16 1zm7.895 22.954l-9.526-5.5a1.5 1.5 0 0 1-.743-1.154l-.007-.145v-11h3V16.29l8.776 5.067-1.5 2.598z"></path>
+                </svg>
+                <span>Commited to AirBnB's enhanced</span>
+              </li>
+            </ul>
+            <a href="#">
+              <span>Show All</span>
+              <i className="fa fa-chevron-right"></i>
+            </a>
+          </ThingsToKnowCol>
+          <ThingsToKnowCol>
+            <h3>Cancellation Policy</h3>
+            <p>Add your trip dates</p>
+            <a href="#">
+              <span>Add Dates</span>
+              <i className="fa fa-chevron-right"></i>
+            </a>
+          </ThingsToKnowCol>
+        </ThingsToKnowRow>
+      </div>
+    </>
+  );
 }
